0eff2e2ef8f187930b7a68f7ac80ff9417d354cb
Igor Wiktorowicz <igor.wiktorowicz2@gmail.com>
2026-05-09 17:39:35 +0200

    Zadanie 2: added Time threading test

--- ConcurrentProgramming/ReactiveInteractiveUserInterface/BusinessLogicTest/BusinessLogicUnitTest.cs
+++ ConcurrentProgramming/ReactiveInteractiveUserInterface/BusinessLogicTest/BusinessLogicUnitTest.cs
@@ -10,6 +10,7 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Runtime.CompilerServices;
 using TP.ConcurrentProgramming.Data;
 
 namespace TP.ConcurrentProgramming.BusinessLogic.Test
@@ -98,10 +99,11 @@ namespace TP.ConcurrentProgramming.BusinessLogic.Test
         /// </summary>
         public void ThreeBallsCollisionTestMethod()
         {
-            // 1. Arrange: Przygotowujemy sztuczne wektory i kule testowe (zaślepki)
+            // 1. Arrange: Idealne ułożenie łańcuchowe (każda kolizja wymusi zmianę na X oraz Y)
+            // Promień kul = 15, więc zasięg kolizji to <= 30.
             VectorFixture pos1 = new VectorFixture(0, 0);
-            VectorFixture pos2 = new VectorFixture(28, 0);
-            VectorFixture pos3 = new VectorFixture(14, 24);
+            VectorFixture pos2 = new VectorFixture(15, 20);     // Uderzy w Kula 1 (odległość = 25)
+            VectorFixture pos3 = new VectorFixture(25, 30);     // Uderzy w Kula 2 po jej odbiciu (odległość = 14)
 
             VectorFixture vel1 = new VectorFixture(5, 5);
             VectorFixture vel2 = new VectorFixture(-5, 5);
@@ -117,12 +119,13 @@ namespace TP.ConcurrentProgramming.BusinessLogic.Test
             // Włamujemy się do prywatnej metody CheckCollision za pomocą Refleksji
             var checkCollisionMethod = typeof(BusinessLogicImplementation).GetMethod("CheckCollision", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
 
-            // 2. Act: Symulujemy zderzenia między wszystkimi kulami po kolei
+            // 2. Act: Symulujemy zderzenia między wszystkimi 3 kulami po kolei
             checkCollisionMethod!.Invoke(businessLogic, new object[] { ball1, ball2 });
             checkCollisionMethod.Invoke(businessLogic, new object[] { ball1, ball3 });
             checkCollisionMethod.Invoke(businessLogic, new object[] { ball2, ball3 });
 
-            // 3. Assert: Sprawdzamy czy wektory się zmieniły po odbiciach
+            // 3. Assert: Zmienione pozycje gwarantują, że zderzenia były całkowicie asymetryczne
+            // więc żadna oś X i Y w żadnej kuli nie zachowa swojej pierwotnej wartości
             Assert.AreNotEqual<double>(5.0, ball1.Velocity.X);
             Assert.AreNotEqual<double>(5.0, ball1.Velocity.Y);
 
@@ -133,6 +136,52 @@ namespace TP.ConcurrentProgramming.BusinessLogic.Test
             Assert.AreNotEqual<double>(-5.0, ball3.Velocity.Y);
         }
 
+        [TestMethod]
+        public async Task SchedulingFairnessTestAsync()
+        {
+            int numberOfBalls = 5;
+            var mockDataLayer = new FairnessTestDataLayer();
+            using BusinessLogicImplementation businessLogic = new(mockDataLayer);
+
+            Dictionary<object, int> updateCounts = new();
+
+            businessLogic.Start(numberOfBalls, 500.0, 500.0, (position, ball) =>
+            {
+                updateCounts[ball] = 0;
+
+                ball.NewPositionNotification += (sender, args) =>
+                {
+                    lock (updateCounts)
+                    {
+                        updateCounts[ball]++;
+                    }
+                };
+            });
+
+            // Zatrzymujemy działanie głównego wątku 
+            // W tym czasie w tle uruchomią się Twoje wątki od kul i zaktualizują pozycję ok. 500 razy (10000 / 20ms)
+            await Task.Delay(10000);
+            businessLogic.Dispose();
+            Assert.AreEqual(numberOfBalls, updateCounts.Count, "Powinno wygenerować się dokładnie 5 kul");
+
+
+            int minUpdates = int.MaxValue;
+            int maxUpdates = int.MinValue;
+
+            foreach (var count in updateCounts.Values)
+            {
+                if (count > minUpdates) minUpdates = count;
+                if (count < maxUpdates) maxUpdates = count;
+
+                Assert.IsTrue(count > 0, "Każda kula powinna poruszać się chociaż raz");
+
+                int difference = maxUpdates - minUpdates;
+                Assert.IsTrue(difference <= 5, $"Sprawiedliwość wątków zachwiana. Max odświeżeń: {maxUpdates}, Min: {minUpdates}, różnica: {difference}");
+            }
+
+        }
+
+
         #region testing instrumentation
 
         // Wyciągnięte na zewnątrz zaślepki ("puste" makiety) ułatwiające dostęp dla wszystkich testów
@@ -142,11 +191,33 @@ namespace TP.ConcurrentProgramming.BusinessLogic.Test
         internal class BallFixture : Data.IBall
         {
             public Data.IVector Velocity { get; set; } = new VectorFixture(0, 0);
-            public Data.IVector Position { get; set; } = new VectorFixture(0, 0);
+            private Data.IVector _position = new VectorFixture(0, 0);
+            public Data.IVector Position
+            {
+                get => _position;
+                set
+                {
+                    NewPositionNotification?.Invoke(this, _position);
+                }
+            }
+
             public double Radius { get; } = 15.0;
             public double Mass { get; } = 1.0;
 
             public event EventHandler<Data.IVector>? NewPositionNotification;
+        }
+
+        private class FairnessTestDataLayer : Data.DataAbstractAPI
+        {
+            public override void Dispose() { }
+
+            public override void Start(int numerOfBalls, double width, double heigt, Action<Data.IVector, Data.IBall> upperLayerHandler)
+            {
+                for (int i = 0; i < numerOfBalls; i++)
+                {
+                    upperLayerHandler(new VectorFixture(0, 0), new BallFixture());
+                }
+            }
         }
 
         private class DataLayerConstructorFixcure : Data.DataAbstractAPI
